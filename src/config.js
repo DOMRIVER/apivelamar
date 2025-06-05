17d46a93f138e295445672d26e357a5d5e0a0933
DOMRIVER <bsdrbrand@gmail.com>
2025-06-04 21:42:38 -0500

    Se actualizo

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 import {config} from 'dotenv'
 config()
 
-export const BD_HOST=process.env.BD_HOST || 'sql107.infinityfree.com'
-export const BD_DATABASE=process.env.BD_DATABASE || 'if0_39153125_velamar'
-export const BD_USER=process.env.BD_USER || 'if0_39153125'
-export const BD_PASSWORD=process.env.BD_PASSWORD || 'JVZUd2Xa7JxD'
+export const BD_HOST=process.env.BD_HOST || 'bpsf9huy8bpext0vlkzm-mysql.services.clever-cloud.com'
+export const BD_DATABASE=process.env.BD_DATABASE || 'bpsf9huy8bpext0vlkzm'
+export const BD_USER=process.env.BD_USER || 'ukflyyj7xwxmttqm'
+export const BD_PASSWORD=process.env.BD_PASSWORD || 'NHrBXRnmsqhJOCrTUXcG'
 export const BD_PORT=process.env.BD_PORT || 3306
 export const PORT=process.env.PORT || 3000
